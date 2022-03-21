019d57842aa55c0c16b2906557124825f4bb0193
alphardex <2582347430@qq.com>
2022-03-21 14:49:29 +0800

    add triplanarMapping

---
+++ triplanarMapping.glsl
@@ -0,0 +1,17 @@
+vec3 blendNormal(vec3 normal){
+    vec3 blending=abs(normal);
+    blending=normalize(max(blending,.00001));
+    blending/=vec3(blending.x+blending.y+blending.z);
+    return blending;
+}
+
+// https://gamedevelopment.tutsplus.com/articles/use-tri-planar-texture-mapping-for-better-terrain--gamedev-13821
+vec3 triplanarMapping(sampler2D tex,vec3 normal,vec3 position){
+    vec3 normalBlend=blendNormal(normal);
+    vec3 xColor=texture2D(tex,position.yz).rgb;
+    vec3 yColor=texture2D(tex,position.xz).rgb;
+    vec3 zColor=texture2D(tex,position.xy).rgb;
+    return(xColor*normalBlend.x+yColor*normalBlend.y+zColor*normalBlend.z);
+}
+
+#pragma glslify:export(triplanarMapping)
