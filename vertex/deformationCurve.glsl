317a08ca393c63b9a6faf1bd4989c4f2d68bd9c2
alphardex <2582347430@qq.com>
2022-04-10 13:50:29 +0800

    update structure

--- vertex/deformationCurve.glsl
+++ vertex/deformationCurve.glsl
@@ -1,4 +1,4 @@
-#pragma glslify:PI=require(glsl-constants/PI)
+const float PI=3.14159265359;
 
 // https://tympanus.net/codrops/2019/10/21/how-to-create-motion-hover-effects-with-image-distortions-using-three-js/
 vec3 deformationCurve(vec3 position,vec2 uv,vec2 offset){
