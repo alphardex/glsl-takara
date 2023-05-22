cde89d4909f51044b2a230bc38769d0508dfadab
alphardex <2582347430@qq.com>
2023-05-22 10:07:00 +0800

    update

--- operation/steppedVal.glsl
+++ operation/steppedVal.glsl
@@ -2,4 +2,8 @@ float steppedVal(float v,float steps){
     return floor(v*steps)/steps;
 }
 
+vec2 steppedVal(vec2 v,float steps){
+    return floor(v*steps)/steps;
+}
+
 #pragma glslify:export(steppedVal)
